42a452cb7c610214162d9ee6f5b05dd259fc633d
loudy <loudy@zonneplan.nl>
2022-10-12 21:08:44 +0200

    Added SocialIcons to the MainScreen

--- src/Screens/MainScreen.tsx
+++ src/Screens/MainScreen.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
 import RoundaboutsLogo from '../Components/RoundaboutsLogo';
+import SocialIcon from '../Components/Social/SocialIcon';
 import VideoBackground from '../Components/VideBackground';
 
 function MainScreen() {
   return (
     <Container>
       <VideoBackground />
+
+      <Header></Header>
+
       <Content>
         <RoundaboutsLogo />
       </Content>
+
+      <Footer>
+        <SocialIcons>
+          <SocialIcon
+            name="facebook"
+            url="https://www.facebook.com/roundaboutsband"
+            icon="IoLogoFacebook"
+          />
+          <SocialIcon
+            name="instagram"
+            url="https://www.instagram.com/roundaboutsband"
+            icon="IoLogoInstagram"
+          />
+          <SocialIcon
+            name="tiktok"
+            url="https://www.tiktok.com/@roundaboutsband"
+            icon="IoLogoTiktok"
+          />
+          <SocialIcon
+            name="youtbube"
+            url="https://www.youtube.com/channel/UCbJAWKjqUS1ZxJYf7PMcemA"
+            icon="IoLogoYoutube"
+          />
+        </SocialIcons>
+      </Footer>
     </Container>
   );
 }
@@ -19,6 +48,11 @@ const Container = styled.div`
     width: 100%;
     display: flex;
     flex: 1;
+    flex-direction: column;
+`;
+
+const Header = styled.div`
+  flex: 1;
 `;
 
 const Content = styled.div`
@@ -26,6 +60,18 @@ const Content = styled.div`
     flex: 1;
     align-items: center;
     justify-content: center;
+`;
+
+const Footer = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+const SocialIcons = styled.div`
+  display: flex;
 `;
 
 export default MainScreen;
