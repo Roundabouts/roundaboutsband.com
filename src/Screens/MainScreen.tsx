443019cf99c8cbc07e572a22314a2d6518664132
loudy <loudy@zonneplan.nl>
2022-10-17 20:39:26 +0200

    Animated social icons

--- src/Screens/MainScreen.tsx
+++ src/Screens/MainScreen.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import { motion, Variants } from 'framer-motion';
 import RoundaboutsLogo from '../Components/RoundaboutsLogo';
 import SocialIcon from '../Components/Social/SocialIcon';
 import VideoBackground from '../Components/VideBackground';
+
+const item: Variants = {
+  hidden: { y: '0.3em', opacity: 0 },
+  visible: { y: 0, opacity: 1, transition: { duration: 1 } },
+};
 
 function MainScreen() {
   return (
@@ -12,43 +18,68 @@ function MainScreen() {
       <Header></Header>
 
       <Content>
-        <RoundaboutsLogo />
+        <motion.div
+          initial={{ opacity: 0, scale: 0.86 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1, delay: 1 }}
+        >
+          <RoundaboutsLogo />
+        </motion.div>
       </Content>
 
       <Footer>
-        <SocialIcons>
-          <SocialIcon
-            name="facebook"
-            url="https://www.facebook.com/roundaboutsband"
-            icon="IoLogoFacebook"
-          />
-          <SocialIcon
-            name="instagram"
-            url="https://www.instagram.com/roundaboutsband"
-            icon="IoLogoInstagram"
-          />
-          <SocialIcon
-            name="tiktok"
-            url="https://www.tiktok.com/@roundaboutsband"
-            icon="IoLogoTiktok"
-          />
-          <SocialIcon
-            name="youtbube"
-            url="https://www.youtube.com/channel/UCbJAWKjqUS1ZxJYf7PMcemA"
-            icon="IoLogoYoutube"
-          />
-        </SocialIcons>
+        <AnimatedSocialIconsContainer
+            initial="hidden"
+            animate="visible"
+            variants={{
+              visible: {
+                transition: {
+                  delayChildren: 2,
+                  staggerChildren: 0.3,
+                },
+              },
+            }}
+        >
+          <AnimatedSocialIcon variants={item}>
+            <SocialIcon
+              name="facebook"
+              url="https://www.facebook.com/roundaboutsband"
+              icon="IoLogoFacebook"
+            />
+          </AnimatedSocialIcon>
+          <AnimatedSocialIcon variants={item}>
+            <SocialIcon
+              name="instagram"
+              url="https://www.instagram.com/roundaboutsband"
+              icon="IoLogoInstagram"
+            />
+          </AnimatedSocialIcon>
+          <AnimatedSocialIcon variants={item}>
+            <SocialIcon
+              name="tiktok"
+              url="https://www.tiktok.com/@roundaboutsband"
+              icon="IoLogoTiktok"
+            />
+          </AnimatedSocialIcon>
+          <AnimatedSocialIcon variants={item}>
+            <SocialIcon
+              name="youtbube"
+              url="https://www.youtube.com/channel/UCbJAWKjqUS1ZxJYf7PMcemA"
+              icon="IoLogoYoutube"
+            />
+          </AnimatedSocialIcon>
+        </AnimatedSocialIconsContainer>
       </Footer>
     </Container>
   );
 }
 
 const Container = styled.div`
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex: 1;
-    flex-direction: column;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
 `;
 
 const Header = styled.div`
@@ -56,22 +87,32 @@ const Header = styled.div`
 `;
 
 const Content = styled.div`
-    display: flex;
-    flex: 1;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Footer = styled.div`
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
 `;
 
-const SocialIcons = styled.div`
+const AnimatedSocialIconsContainer = styled(motion.ul)`
   display: flex;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  `;
+
+const AnimatedSocialIcon = styled(motion.li)`
+  margin: 0;
+  padding: 0;
 `;
+
+
 
 export default MainScreen;
