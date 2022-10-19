1a08e0291db66662ba53e252ec46f5833939a2be
loudy <loudy@zonneplan.nl>
2022-10-19 22:29:08 +0200

    Migrated to NextJS

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/app.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        {/* <meta charset="utf-8" /> */}
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#000000" />
+
+        <meta name="description" content="Web site created using create-react-app"/>
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        
+        <title>Roundabouts</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
