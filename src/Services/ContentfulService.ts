a8255af9030cb898fd960760eca234c69b438d6a
loudy <loudy@zonneplan.nl>
2023-08-08 18:00:27 +0200

    Created contentful api service

--- src/Services/ContentfulService.ts
+++ src/Services/ContentfulService.ts
@@ -1,31 +1,59 @@
-import { createClient } from 'contentful'; 
-import { TypeEventFields, TypeEventSkeleton } from '../Types/Contentful';
+import { Entry, createClient } from 'contentful'; 
+import { IEvent, IEventFields, IWebsite } from '../@types/contentful';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID ?? '',
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? '',
 });
 
-export interface Event {
-    title: string;
-    date: string;
-    location?: any;
-    url: string | null;
-    ticketsUrl: string | null;
-    type: 'gig';
+export interface WebsiteData {
+  title: string;
+  description: string;
 }
 
-export async function getEvents(): Promise<Array<Event>> {
-  const response = await client.getEntries<TypeEventSkeleton>({
-    content_type: 'event',
+export interface Event {
+  title: string;
+  date: string;
+  location?: any;
+  url: string | null;
+  ticketsUrl: string | null;
+  type: 'gig';
+}
+
+class ContentfulParser {
+  static parseWebsiteData(entry: IWebsite): WebsiteData {
+    return {
+      title: entry.fields.title,
+      description: entry.fields.description,
+    };
+  }
+
+  static parseEvent(entry: IEvent): Event {
+    return {
+      title: entry.fields.title,
+      date: entry.fields.date,
+      location: entry.fields.location,
+      url: entry.fields.url || null,
+      ticketsUrl: entry.fields.ticketsUrl || null,
+      type: entry.fields.type,
+    };
+  }
+}
+
+export async function getWebsiteData() {
+  const response = await client.getEntries<IEvent>({
+    content_type: 'website',
   });
 
-  return response.items.map(({ fields }) => ({
-    title: fields.title,
-    date: fields.date,
-    location: fields.location,
-    url: fields.url || null,
-    ticketsUrl: fields.ticketsUrl || null,
-    type: fields.type,
-  }));
+  return ContentfulParser.parseWebsiteData(response.items[0] as any);
 }
+
+export async function getEvents() {
+  const response = await client.getEntries<IEvent>({
+    content_type: 'event',
+    order: 'fields.date',
+  });
+
+  return response.items.map(item => ContentfulParser.parseEvent(item as any));
+}
+
