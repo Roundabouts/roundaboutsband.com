b744e01b6ec6064656bae3ff29c2fa1a82cc5623
loudy <loudy@zonneplan.nl>
2023-08-08 17:59:33 +0200

    Added autoPlay prop

--- src/Components/VideoBackground.tsx
+++ src/Components/VideoBackground.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const VideoBackground = () => (
+interface Props {
+    autoPlay?: boolean;
+}
+
+const VideoBackground = ({
+    autoPlay = true,
+}: Props) => (
     <div className="App">
         <Background>
-            <Video autoPlay muted loop>
+            <Video autoPlay={autoPlay} muted loop>
                 <source src="videos/promo-midnite.mp4" type="video/mp4" />
                 <source src="videos/promo-midnite.webm" type="video/webm" />
             </Video>
