d82197a5637482afe5b24d708994940cf0605e53
loudy <loudy@zonneplan.nl>
2024-02-22 16:41:59 +0100

    Fixed date format

--- src/Utils/FormatUtils.ts
+++ src/Utils/FormatUtils.ts
@@ -1,10 +1,9 @@
 import format from 'date-fns/format';
 import parseISO from 'date-fns/parseISO';
-import { nl } from 'date-fns/locale'
 
 export class FormatUtils {
     static formatIsoDate(isoDate: string, dateFormat: string): string {
         const date = parseISO(isoDate);
-        return format(date, dateFormat, { locale: nl });
+        return format(date, dateFormat);
     }
 }
