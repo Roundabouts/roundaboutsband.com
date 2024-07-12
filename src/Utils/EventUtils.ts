b0c584f6f5924b2f6c61a41d37522fcadfeb8d6e
loudy <loudy@zonneplan.nl>
2024-07-12 13:07:48 +0200

    Fixed todays events

--- src/Utils/EventUtils.ts
+++ src/Utils/EventUtils.ts
@@ -1,10 +1,11 @@
 import { Event } from '../Services/ContentfulService';
 import isFuture from 'date-fns/isFuture';
+import isToday from 'date-fns/isToday';
 import parseISO from 'date-fns/parseISO';
 
 export class EventUtils {
     static isFutureEvent(event: Event): boolean {
         const eventDate = parseISO(event.date);
-        return isFuture(eventDate);
+        return isFuture(eventDate) || isToday(eventDate);
     }
 }
